2f89112be4d751189fae66e7d1d00a1b8f357c53
Julianne Attai <julianne.attai@gmail.com>
2021-07-27 21:52:41 -0400

    initial commit

---
+++ bot.py
@@ -0,0 +1,129 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+import discord
+import os
+
+from funfact import *
+
+intents = discord.Intents.all()
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+    
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+
+###WIKIPEDIA SCRAPER###
+channel_facts = "bugtesting"
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.channel.name != channel_facts:
+        print("wrong channel")
+        return
+    if message.content.startswith('$hello'):
+        await message.channel.send("Hello! My name is Standford the Beaver. Ask me about Skule History by using the command $funfact followed by a topic you want to know more about")
+    
+    elif message.content.startswith("$funfact"):
+        print("hi i am fun")
+        subject = message.content.replace('$funfact','').strip()
+
+        fact = get_fun_fact(subject)
+
+        await message.channel.send(fact)
+
+
+###REACTION FOR ROLES###
+##PARAMETERS + CUSTOMIZATION##
+message_react_id = 869395061278933003
+blue_heart_role = 'research team'
+green_heart_role = 'mom'
+pink_heart_role = 'events volunteer'
+purple_heart_role = 'inventory volunteer'
+
+def choose_role(guild, payload):
+    if payload.emoji.name == '💙':
+        print("blue")
+        print(guild.roles)
+        role = discord.utils.get(guild.roles, name = blue_heart_role)
+            
+    elif payload.emoji.name == '💚':
+        print("green")
+        role = discord.utils.get(guild.roles, name = green_heart_role)
+
+    elif payload.emoji.name =="💗":
+        print("pink")
+        role = discord.utils.get(guild.roles, name = pink_heart_role)
+
+    elif payload.emoji.name =="💜":
+        print("purple")
+        role = discord.utils.get(guild.roles,name = purple_heart_role)
+    
+    return role
+
+##REACTION CODE##
+@client.event
+async def on_raw_reaction_add(payload):
+    message_id = payload.message_id
+    if (message_id==message_react_id):
+        print("this is the right message")
+        guild_id = payload.guild_id
+        guild = discord.utils.find(lambda g: g.id==guild_id, client.guilds)
+
+        print(payload.emoji.name)
+        role = choose_role(guild, payload)
+        
+        if role is not None:
+            print(payload.user_id)
+            print(guild.members)
+            member = discord.utils.find(lambda m : m.id ==payload.user_id, guild.members)
+            if member is not None:
+                await member.add_roles(role)
+                print("done")
+            else:
+                print("member not found")
+        else:
+            print("role not found")
+
+
+    print("Hello world")
+
+@client.event
+async def on_raw_reaction_remove(payload):
+    message_id = payload.message_id
+    if (message_id==message_react_id):
+        print("goodbye message")
+        guild_id = payload.guild_id
+        guild = discord.utils.find(lambda g: g.id==guild_id, client.guilds)
+
+        print(payload.emoji.name)
+        role = choose_role(guild, payload)
+
+        
+        if role is not None:
+            print(payload.user_id)
+            print(guild.members)
+            member = discord.utils.find(lambda m : m.id ==payload.user_id, guild.members)
+            if member is not None:
+                await member.remove_roles(role)
+                print("done")
+            else:
+                print("member not found")
+        else:
+            print("role not found")
+
+
+    print("Goodbye world")
+
+
+
+client.run(os.getenv('BOT_TOKEN'))
---
+++ funfact.py
@@ -0,0 +1,46 @@
+import csv
+import random
+
+csv_name = "funfacts.csv"
+
+def get_fun_fact(subject): 
+    keyword = subject.lower()
+    skule_dict = build_dictionary()
+    keys = list(skule_dict.keys())
+
+    if keyword in keys:
+        facts = (skule_dict[keyword]['facts'])
+        fact = random.choice(facts)	
+        return fact
+
+    else:
+        return ("sorry i'm not smart enough yet. check out skulepedia.ca to find what you're looking for")
+
+    #select a fact from the options, create dictionary and randomly pick one
+    #offer skulepedia page for more info
+
+def build_dictionary():
+
+    skule_dict = {}
+    with open(csv_name, mode='r') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+    
+        for row in csv_reader:
+            if "keyword" in row[0]:
+                continue
+            if(row[0] in skule_dict):
+                skule_dict[row[0]]['facts'].append(row[1])
+            else:
+                skule_dict[row[0]]= {'facts' : [row[1]], 'skulepedia':row[2]}
+
+    print(skule_dict)
+
+    print(csv_reader)
+    return skule_dict
+
+
+
+
+if __name__ == "__main__":
+    print(get_fun_fact("iron ring"))
+   
